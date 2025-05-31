0c02a8fb29091d1fb7345461dbba88a370458b1a
Zhafarullah <ahmadzhafarullah@gmail.com>
2025-05-31 13:18:27 +0700

    testing

--- app.py
+++ app.py
@@ -167,4 +167,4 @@ async def find_similar_images(
 if __name__ == '__main__':
     import uvicorn
     port = int(os.environ.get("PORT", 8000)) 
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
